1562619cb13ce6ca529ef136333fd0347019d4bb
Tony Grochow <tony.grochow@la.thueringen.de>
2022-06-08 12:49:31 +0200

    Parses xdomea message patterns and validate against xdomea schema.

--- generator/message_generator.py
+++ generator/message_generator.py
@@ -20,7 +20,7 @@ class ProcessStructureConfig:
     min_number: int
     max_number: int
     process_evaluation: ProcessEvaluationConfig
-    document_structure_config: DocumentStructureConfig
+    document_structure: DocumentStructureConfig
 
 
 class FileEvaluationConfig(Enum):
@@ -33,7 +33,7 @@ class FileStructureConfig:
     min_number: int
     max_number: int
     file_evaluation: FileEvaluationConfig
-    process_structure_config: ProcessStructureConfig
+    process_structure: ProcessStructureConfig
 
 
 @dataclass
@@ -50,9 +50,9 @@ class TestDataConfig:
 
 @dataclass
 class GeneratorConfig:
-    structure_config: FileStructureConfig
-    message_pattern_config: MessagePatternConfig
-    test_data_config: TestDataConfig
+    structure: FileStructureConfig
+    message_pattern: MessagePatternConfig
+    test_data: TestDataConfig
 
 
 class XdomeaMessageGenerator:
@@ -67,12 +67,11 @@ class XdomeaMessageGenerator:
         config_root = etree.parse(config_path)
         config_schema_root = etree.parse(config_schema_path)
         config_schema = etree.XMLSchema(config_schema_root)
-        assert config_schema.validate(config_root),\
-            'Schemaprüfung Konfigurationsdatei: ' + config_path + ' invalide'
+        config_schema.assertValid(config_root)
         self.config = GeneratorConfig(
-            structure_config=self.__read_structure_config(config_root),
-            message_pattern_config=self.__read_message_pattern_config(config_root),
-            test_data_config=self.__read_test_data_config(config_root),
+            structure=self.__read_structure_config(config_root),
+            message_pattern=self.__read_message_pattern_config(config_root),
+            test_data=self.__read_test_data_config(config_root),
         )
         self.__validate_config()
 
@@ -102,13 +101,13 @@ class XdomeaMessageGenerator:
             min_number=processes_min_number,
             max_number=processes_max_number,
             process_evaluation=processes_evaluation,
-            document_structure_config=document_structure_config,
+            document_structure=document_structure_config,
         )
         return FileStructureConfig(
             min_number=files_min_number,
             max_number=files_max_number,
             file_evaluation=files_evaluation,
-            process_structure_config=process_structure_config,
+            process_structure=process_structure_config,
         )
 
     def __read_message_pattern_config(self, config_root: etree.Element) -> MessagePatternConfig:
@@ -142,20 +141,32 @@ class XdomeaMessageGenerator:
         Validates parsed config. Checks the conditions which the schema validation couldn't check.
         Checks cross field conditions.
         """
-        assert self.config.structure_config.min_number <= self.config.structure_config.max_number,\
+        assert self.config.structure.min_number <= self.config.structure.max_number,\
             'Strukturkonfiguration: maximale Aktenzahl ist kleiner als minimale Aktenzahl'
-        assert self.config.structure_config.process_structure_config.min_number <=\
-            self.config.structure_config.process_structure_config.max_number,\
+        assert self.config.structure.process_structure.min_number <=\
+            self.config.structure.process_structure.max_number,\
             'Strukturkonfiguration: maximale Vorgangszahl ist kleiner als minimale Vorgangszahl'
-        assert self.config.structure_config.process_structure_config.\
-            document_structure_config.min_number <= self.config.structure_config.\
-            process_structure_config.document_structure_config.max_number,\
+        assert self.config.structure.process_structure.\
+            document_structure.min_number <= self.config.structure.\
+            process_structure.document_structure.max_number,\
             'Strukturkonfiguration: maximale Dokumentenzahl ist kleiner als minimale Dokumentenzahl'
+
+    def generate_xdomea_messages(self):
+        """
+        Parses xdomea message patterns and validates against the pattern schema.
+        """
+        pattern_schema_root = etree.parse(self.config.message_pattern.schema_path)
+        pattern_schema = etree.XMLSchema(pattern_schema_root)
+        xdomea_0501_pattern_root = etree.parse(self.config.message_pattern.xdomea_0501_path)
+        pattern_schema.assertValid(xdomea_0501_pattern_root)
+        xdomea_0503_pattern_root = etree.parse(self.config.message_pattern.xdomea_0503_path)
+        pattern_schema.assertValid(xdomea_0503_pattern_root)
 
 
 def main():
     message_generator = XdomeaMessageGenerator()
     message_generator.read_config('config/generator_config.xml', 'config/generator_config.xsd')
+    message_generator.generate_xdomea_messages()
 
 
 if __name__ == '__main__':
