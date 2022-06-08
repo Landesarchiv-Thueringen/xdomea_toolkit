39e57ba4580567d190463d7eb9999a0cb0111a20
Tony Grochow <tony.grochow@la.thueringen.de>
2022-06-08 12:04:59 +0200

    clear

--- generator/message_generator.py
+++ generator/message_generator.py
@@ -59,6 +59,11 @@ class XdomeaMessageGenerator:
     config: GeneratorConfig
 
     def read_config(self, config_path: str, config_schema_path: str):
+        """
+        Parses xml config file into object representation. Validates xml config file against schema.
+        :param config_path: path of xml config file
+        :param config_schema_path: path of xml schema for config file
+        """
         config_root = etree.parse(config_path)
         config_schema_root = etree.parse(config_schema_path)
         config_schema = etree.XMLSchema(config_schema_root)
@@ -72,6 +77,11 @@ class XdomeaMessageGenerator:
         self.__validate_config()
 
     def __read_structure_config(self, config_root: etree.Element) -> FileStructureConfig:
+        """
+        Parses message structure config into object representation.
+        :param config_root: xml root of config
+        :return: file structure config
+        """
         files_min_number = int(config_root.findtext('/structure/files/min_number'))
         files_max_number = int(config_root.findtext('/structure/files/max_number'))
         files_evaluation = FileEvaluationConfig[config_root.findtext(
@@ -102,6 +112,11 @@ class XdomeaMessageGenerator:
         )
 
     def __read_message_pattern_config(self, config_root: etree.Element) -> MessagePatternConfig:
+        """
+        Parses message pattern config into object representation.
+        :param config_root: xml root of config
+        :return: message pattern config
+        """
         xdomea_0501_path = config_root.findtext('/message_pattern/xdomea_0501_path')
         xdomea_0503_path = config_root.findtext('/message_pattern/xdomea_0503_path')
         schema_path = config_root.findtext('/message_pattern/schema_path')
@@ -112,12 +127,21 @@ class XdomeaMessageGenerator:
         )
 
     def __read_test_data_config(self, config_root: etree.Element) -> TestDataConfig:
+        """
+        Parses test data config into object representation.
+        :param config_root: xml root of config
+        :return: test data config
+        """
         test_data_root_dir = config_root.findtext('/test_data/root_dir')
         return TestDataConfig(
             root_dir=test_data_root_dir,
         )
 
     def __validate_config(self):
+        """
+        Validates parsed config. Checks the conditions which the schema validation couldn't check.
+        Checks cross field conditions.
+        """
         assert self.config.structure_config.min_number <= self.config.structure_config.max_number,\
             'Strukturkonfiguration: maximale Aktenzahl ist kleiner als minimale Aktenzahl'
         assert self.config.structure_config.process_structure_config.min_number <=\
